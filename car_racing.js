356d5a5412c26f4607cb968f4fbb6bc0c5c278b2
Ayush yadav <146109636+ARHW0304@users.noreply.github.com>
2023-10-29 16:40:14 +0530

    Update car_racing.js

--- car_racing.js
+++ car_racing.js
@@ -1,1 +1,5 @@
-
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.highScore');
+const startScreen = document.querySelector('.startScreen');
+const gameArea = document.querySelector('.gameArea');
+const ClickToStart = document.querySelector('.ClickToStart');
